6b964a0fd7b330bf0c3759c6abdfe790d501eb64
Alex Soble <asoble@gmail.com>
2016-06-25 17:09:49 -0500

    Write logic for storing results of page scrape, avoid duplicates

--- scrape_page.js
+++ scrape_page.js
@@ -1,17 +1,31 @@
 function scrapePage (tripRows) {
 
   // Let's start by fetching our trips list from local storage!
+  chrome.storage.sync.get('tripsDataArray', function(data) {
+    if ($.isEmptyObject(data)) { data = []; }
 
-  // For each row in the trip rows ...
-     // Add it to the trips list array if its id isn't already in the array
+    const storedTripIds = data.map(function(trip) {
+      return trip['trip_id'];
+    });
 
-  // Then when all is done, update the trips list array in local storage
+    // For each row in the trip rows ...
+       // Add it to the trips list array if its id isn't already in the array.
+       // (We can tell because of the trip ID.)
 
-  tripRows.each(function(row) {
-    let dataCells = $(this).children();
-    let rowTripData = (new TripHtmlRow(dataCells)).toJsObject();
+    tripRows.each(function(row) {
+      let dataCells = $(this).children();
+      let rowTripData = (new TripHtmlRow(dataCells)).toJsObject();
+      let trip_id = rowTripData['trip_id'];
+      let isNotYetStored = (storedTripIds.indexOf(trip_id) === -1);
 
-    pageTripsData.push(rowTripData);
+      if (isNotYetStored) {
+        data.push(rowTripData);
+      }
+    });
+
+    // Then when all is done, update the trips data array in local storage:
+    chrome.storage.sync.set({ 'tripsDataArray': data }, function() {});
+
   });
 
 };
