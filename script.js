624a093fc9f8eb44d49f968107a87f8e63a3db3d
Alex Soble <asoble@gmail.com>
2016-06-25 16:35:34 -0500

    Get total number of trips for user

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 $(function() {
+  // First, detect if the page has a trip table element.
+  // If no, there's a problem on the page, quit:
 
   const tripTable = $("#tripTable");
-
   if (tripTable.length !== 1) return;
+
+  // If yes, look up total # of trips and add it to data:
+  const totalTrips = findTotalTrips($('#content'));
+  console.log(totalTrips);
+
+// Compare total number of trips with trips stored in data
+
+  // If the number is equal:
+
+    // The user can do stuff with their data!
+    // Option 1: export as CSV or JSON
+    // Option 2: send it to DivvyBrags server for viz?
+
+  // If the number is not equal:
+
+    // User needs to go scrape more trips!
+    // For each page, get user data
 
   const tripRows= $("#tripTable tbody tr");
 
@@ -18,21 +36,4 @@ $(function() {
   console.log(pageTripsData);
 });
 
-// Detect if the page is a trip list
-  // If not, do nothing
 
-// If yes, look up total # of trips and add it to data
-
-// Compare total number of trips with trips stored in data
-
-  // If the number is equal:
-
-    // The user can do stuff with their data!
-    // Option 1: export as CSV or JSON
-    // Option 2: send it to DivvyBrags server for viz?
-
-  // If the number is not equal:
-
-    // User needs to go scrape more trips!
-    // For each page, present a "SCRAPE MY TRIPS" button
-      // Clicking the page lets the user get more trips into their data
