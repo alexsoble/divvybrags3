1c473062717a09c3741790d32c560208d4c0e891
Alex Soble <asoble@gmail.com>
2016-07-01 14:38:18 -0500

    We should use our own server instead of Chrome storage

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 $(function() {
-  // First, detect if the page has a trip table element.
-  // If no, there's a problem on the page, quit:
 
   const tripTable = $("#tripTable");
   if (tripTable.length !== 1) return;
 
-  // If yes, look up total # of trips and store it to data:
-  const totalTrips = findTotalTrips($('#content'));
-  chrome.storage.sync.set({ 'totalTripsCount': totalTrips }, function() {});
-
-  chrome.storage.sync.get('tripsDataArray', function(data) {
-    let unwrapper = new ChromeStorageUnwrapper(data);
-    unwrappedData = unwrapper.unwrappedData();
-
-    let tripsDataSize = unwrappedData.length || 0;
-
-    // Compare total number of trips with trips stored in data:
-    if (totalTrips > tripsDataSize) {
-      // User needs to go scrape more trips!
-      // For each page, get user data
-
-      alert('Dearest user, we are going to need some more trip data for you!');
-
-      const tripRows = $("#tripTable tbody tr");
-
-      scrapePage(tripRows);
-
+  chrome.storage.sync.get('userDivvyBragsId', function(data) {
+    if ($.isEmptyObject(data)) {
+      const userDivvyBragsId = Math.random().toString(36).substring(7);
+      chrome.storage.sync.set({ 'userDivvyBragsId': userDivvyBragsId }, function() {});
     } else {
-      // The user can do stuff with their data!
-      // Option 1: export as CSV or JSON
-      // Option 2: send it to DivvyBrags server for viz?
+      const userDivvyBragsId = data.userDivvyBragsId;
+    }
 
-      alert('Dearest user, this is great, you can do all sorts of fun things with your data!');
-    };
+    $.ajax({
+      url: 'divvybrags-server.herokuapp.com',
+      data: userDivvyBragsId,
+      success: function() {
+        const totalTrips = findTotalTrips($('#content'));
+        let tripsDataSize = unwrappedData.length || 0;
+
+        // Compare total number of trips with trips stored in data:
+        if (totalTrips > tripsDataSize) {
+          // User needs to go scrape more trips!
+          // For each page, get user data
+
+          alert('Dearest user, we are going to need some more trip data for you!');
+
+          const tripRows = $("#tripTable tbody tr");
+
+          scrapePage(tripRows);
+        } else {
+          // The user can do stuff with their data!
+          // Option 1: export as CSV or JSON
+          // Option 2: send it to DivvyBrags server for viz?
+
+          alert('Dearest user, this is great, you can do all sorts of fun things with your data!');
+        };
+      }
+    });
+
   });
+
 });
 
 
